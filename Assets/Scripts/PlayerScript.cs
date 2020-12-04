c6354a8ad6b1fb7aa837d972e10c7ad8d0045562
Shanice Smith <shanicesmith.work@hotmail.com>
2020-12-03 20:08:01 -0500

    added project files

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    public AudioClip [] soundClips;
+    // 0 - Shooting
+    // 1 - Explosion
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireBullet();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerExplosion();
+        }
+    }
+
+    void TriggerExplosion()
+    {
+        audioSource.PlayOneShot(soundClips[1]);
+    }
+
+    void FireBullet()
+    {
+        audioSource.PlayOneShot(soundClips[0]);
+    }
+}
