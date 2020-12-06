6af9627ad02aa692175863dccf80ac694ae05362
Shanice Smith <shanicesmith.work@hotmail.com>
2020-12-06 07:07:12 -0500

    added movement across screen

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float timer = 0;
+    float timeToMove = 0.5f;
+
+    public GameObject missile;
+    public GameObject missileClone;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > timeToMove)
+        {
+            transform.Translate(new Vector3(0.1f, 0f, 0));
+            timer = 0;
+        }
     }
 }
